497a44ba6dba919a9f78bd05c4115c1c570ec0e8
Abdullah Kadioglu <abdkadioglu21@gmail.com>
2022-10-12 19:16:48 +0300

    tt

--- src/test/java/githuup/Calisma.java
+++ src/test/java/githuup/Calisma.java
@@ -4,8 +4,10 @@ public class Calisma {
 
     public static void main(String[] args) {
         System.out.println("hillli lllihhh");
-        System.out.println("Sirin");
+        System.out.println("Sirin.");
+        System.out.println("abdullah");
+        //aaaaaaaa
+
 
     }
-
 }
